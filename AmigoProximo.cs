249420ac4647da9afd9902e3317e39cff91ab062
ALESON GOMES DE MELO <aleson.gmelo@senacsp.edu.br>
2024-11-06 11:52:38 -0300

    Atv de encapsulamento

---
+++ AmigoProximo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento {
+    internal class AmigoProximo  {
+
+        //amigo próximo vai estar dentro do mesmo projeto
+        //porem não vai ter aquela relação de herança
+        public readonly SubCelebridade amigo = new SubCelebridade();
+
+        public void MeusAcessos() {
+
+            Console.WriteLine("Amigo Próximo... ");
+            Console.WriteLine(amigo.InfoPublica);
+            //Console.WriteLine(amigo.CorDoOlho);
+            Console.WriteLine(amigo.NumeroCelular);
+            Console.WriteLine(amigo.JeitoDeFalar.);
+            //Console.WriteLine(amigo.Segredo);
+            //Console.WriteLine(amigo.PegaAsMeninasDoEmed);
+        }
+    }
+}
---
+++ SubCelebridade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento {
+    class SubCelebridade {
+        //esse atributo é acessado por todos 
+        public string InfoPublica = "Tenho Instagram";
+
+        //esse segundo atributo é transmitido por herança 
+        protected string CorDoOlho = "Castanho";
+
+        //só pode acessar se for do mesmo projeto
+        internal ulong NumeroCelular = 5511947928788;
+
+        // atributo transmitindo por herença ou no mesmo projeto
+        protected internal string JeitoDeFalar = "Fala muitas falacias";
+
+        //somente pode ser transmitido no mesmo projeto ou por herança(assembly)
+        private protected string Segredo = "Joga no Tigrinho";
+
+        //atributo somente visivel somente nessa classe, é um segredo e ninguem mais pode saber, a não ser a propria pessoa
+        private bool PegaAsMeninasDoEmed = true;
+
+        public void MeusAcessos() {
+            Console.WriteLine("Pedro: ");
+            Console.WriteLine(InfoPublica);
+            Console.WriteLine(CorDoOlho);
+            Console.WriteLine(NumeroCelular);
+            Console.WriteLine(JeitoDeFalar);
+            Console.WriteLine(Segredo);
+            Console.WriteLine(PegaAsMeninasDoEmed);
+        }
+
+    }
+}
